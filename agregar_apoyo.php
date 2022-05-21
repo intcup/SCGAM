40946fd4e54d27d01341bc9154f770e263a3c975
intcup <etini.artur@outlook.com>
2022-05-21 17:15:33 -0500

    Eliminadas las imagenes, agregado registro apoyos

--- agregar_apoyo.php
+++ agregar_apoyo.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
+$db = require("db/connect.php");
 if(!isset($_SESSION["user"])){
 	header("Location: index.php");	
 }
 
 if(isset($_POST['id'])){
-	$db = require("db/connect.php");
 	$stm = $db->prepare("INSERT INTO Apoyos(descripcion,area,ciudadano) VALUES(?,?,?)");
 	$stm->bind_param("sii", $_POST['descripcion'], $_POST['area'], $_POST['id']);
 	$stm->execute();
@@ -21,7 +21,14 @@ echo $_GET['id'];
 ?>"
 type="hidden" name="id">
 <select name="area">
-<option value="1">test</option>
+<?php
+	$areas = $db->prepare("SELECT id, nombre FROM Areas_Apoyos");
+	$areas->execute();
+	$areas->bind_result($id, $nombre);
+	while ( $areas->fetch() ){
+		echo "<option value=\"" . $id . "\">" . $nombre . "</option>";
+	}
+?>
 </select>
 <textarea name="descripcion"></textarea>
 <input type="submit"/>
